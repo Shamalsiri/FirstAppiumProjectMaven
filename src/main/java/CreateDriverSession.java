f7d5b6ff656038c3b47a3d9a931af280bc9483f8
Shamal Siriwardana <shamal.v.siriwardana@gmail.com>
2021-11-17 13:33:27 -0500

    [SS] Imported the Appium Java client dependency on to the pom.xml via using build automation tools. Created the CreateDriverSession class and the main method.

---
+++ src/main/java/CreateDriverSession.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class CreateDriverSession {
+
+    public static void main(Strings[] args)
+    {
+        DesiredCapabilities cap = new DesiredCapabilities();
+    }
+}
