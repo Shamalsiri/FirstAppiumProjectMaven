162896a6999ff6f80a9fe3751b989d6fa4f41945
Shamal Siriwardana <shamal.v.siriwardana@gmail.com>
2021-11-27 00:19:55 -0500

    [SS] Created a new class: FindElements. Moved main method from CreateDriverSession to FindElements

---
+++ src/main/java/FindElements.java
@@ -0,0 +1,7 @@
+import io.appium.java_client.AppiumDriver;
+
+public class FindElements {
+    public static void main(String[] args) throws Exception {
+        AppiumDriver driver = CreateDriverSession.initializeDriver();
+    }
+}
