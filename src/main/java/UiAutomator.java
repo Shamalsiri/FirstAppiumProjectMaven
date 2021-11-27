ed5db06a2532c6aa7d290994ce8e467f66efc232
Shamal Siriwardana <shamal.v.siriwardana@gmail.com>
2021-11-27 01:50:23 -0500

    [SS] Created the UiAutomator class. Renamed the main method in FindElements class to findElements(). Moved main method from FindElements class to UiAutomator class

---
+++ src/main/java/UiAutomator.java
@@ -0,0 +1,7 @@
+import io.appium.java_client.AppiumDriver;
+
+public class UiAutomator {
+    public static void main(String[] args) throws Exception {
+        AppiumDriver driver = CreateDriverSession.initializeDriver();
+    }
+}
