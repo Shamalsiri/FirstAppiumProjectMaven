44b4919358c5564f4e092cb994d5291e708a1de4
Shamal Siriwardana <shamal.v.siriwardana@gmail.com>
2021-11-27 10:52:10 -0500

    [SS] Examples of using UiSelector to find the Elements in the application

--- src/main/java/UiAutomator.java
+++ src/main/java/UiAutomator.java
@@ -1,7 +1,26 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.FindsByAndroidUIAutomator;
+import io.appium.java_client.MobileElement;
 
 public class UiAutomator {
     public static void main(String[] args) throws Exception {
         AppiumDriver driver = CreateDriverSession.initializeDriver();
+
+        MobileElement element = (MobileElement) ((FindsByAndroidUIAutomator)driver)
+                .findElementByAndroidUIAutomator("new UiSelector().text(\"Accessibility\")"); //From text field in the Inspector
+        System.out.println(element.getText());
+
+        element = (MobileElement) ((FindsByAndroidUIAutomator)driver)
+                .findElementsByAndroidUIAutomator("new UiSelector().className(\"android.widget.TextView\")").get(2); //From class field in the Inspector
+                        //more than one element have the same class name so need to use findElement's' instead of findElement and providing the index of element
+        System.out.println(element.getText());
+
+        element = (MobileElement) ((FindsByAndroidUIAutomator)driver)
+                .findElementByAndroidUIAutomator("new UiSelector().description(\"Accessibility\")"); // From content-desc field in the inspector
+        System.out.println(element.getText());
+
+        element = (MobileElement) ((FindsByAndroidUIAutomator)driver)
+                .findElementsByAndroidUIAutomator("new UiSelector().resourceId(\"android:id/text1\")").get(1); // From resource-id field in the inspector
+        System.out.println(element.getText());
     }
 }
